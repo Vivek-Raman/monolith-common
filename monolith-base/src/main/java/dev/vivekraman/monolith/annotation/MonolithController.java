712e3d093a4be9085078d7d15a66c6b86373c220
Vivek Raman <vr.ac4bf@live.com>
2023-11-11 06:44:16 +0530

    Move RequestMapping into MonolithController

--- monolith-base/src/main/java/dev/vivekraman/monolith/annotation/MonolithController.java
+++ monolith-base/src/main/java/dev/vivekraman/monolith/annotation/MonolithController.java
@@ -2,6 +2,8 @@ package dev.vivekraman.monolith.annotation;
 
 import dev.vivekraman.monolith.config.ModuleEnabledCondition;
 import org.springframework.context.annotation.Conditional;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.annotation.ElementType;
@@ -18,6 +20,9 @@ import java.lang.annotation.Target;
  * If {@code spring.profiles.include} contains the module name,
  * then the controllers will be included, else they will be inaccessible.
  * This acts as a kill-switch to all entry-points of each module.
+ * <br />
+ * Additionally, this enforces the base path of the controller to be the
+ * {@link MonolithController#moduleName()}.
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
@@ -25,8 +30,6 @@ import java.lang.annotation.Target;
 @RestController
 @Conditional(ModuleEnabledCondition.class)
 public @interface MonolithController {
-  /**
-   * Used in {@link ModuleEnabledCondition}
-   */
+  @AliasFor(annotation = RequestMapping.class, attribute = "path")
   String moduleName();
 }
